0b15253491c9735db7a57fb79d8ac8fea7deeffe
anoy1729 <51030142+anoy1729@users.noreply.github.com>
2020-10-25 14:26:36 +0600

    Update Python If-Else.py

--- Python If-Else.py
+++ Python If-Else.py
@@ -10,9 +10,9 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    if(n%2!=0 or n in range(6,20)):
+    if(n%2!=0 or n in range(6,21)):
         print('Weird')
-    elif(n in range(2,5) or n>20):
+    elif(n in range(2,6) or n>20):
         print('Not Weird')
 
         
